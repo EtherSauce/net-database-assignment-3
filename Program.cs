3ba75a6bf1ba33309e9b2786f58a76a849dfae1e
Ethan Sitterly <ethan.sitterly@gmail.com>
2025-02-13 13:58:28 -0600

    [1.02]

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+logger.Info("Program started");
+string file = "mario.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+}
+else
+{
+        // create parallel lists of character details
+    // lists are used since we do not know number of lines of data
+    List<UInt64> Ids = [];
+    List<string> Names = [];
+    List<string> Descriptions = [];
+        // to populate the lists with data, read from the data file
+    try
+    {
+        StreamReader sr = new(file);
+        // first line contains column headers
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        sr.Close();
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex.Message);
+    }
+        string? choice;
+    do
+    {
+        // display choices to user
+        Console.WriteLine("1) Add Character");
+        Console.WriteLine("2) Display All Characters");
+        Console.WriteLine("Enter to quit");
+        // input selection
+        choice = Console.ReadLine();
+        logger.Info("User choice: {Choice}", choice);
+        if (choice == "1")
+        {
+            // Add Character
+        }
+        else if (choice == "2")
+        {
+            // Display All Characters
+        }
+    } while (choice == "1" || choice == "2");
+}
+logger.Info("Program ended");
